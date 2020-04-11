60428ac43fc95890a0b3a4e9394160e147546622
jkaplanis <johntkaplanis@gmail.com>
2020-04-10 18:58:08 -0700

    refactor code to serve static webpages with express.static

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,26 +1,16 @@
 // Import the path package to get the correct file path for our html
 const path = require("path");
-
+const express = require("express");
 // Export a function that accepts an Express app object and sets up
 // html routes to server the html files.
 module.exports = app => {
-  // Below code handles when users "visit" a page.
-  // In this case the user is shown an HTML page of content
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-  });
-
-  app.get("/assets/css/styles.css", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/css/styles.css"));
-  });
-
-  app.get("/assets/js/index.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/assets/js/index.js"));
-  });
-  // If no matching route is found default to home. The "*" is a special
-  // character used to match anything. Its important to put this route last!
-  // once Express finds a matching route, it stops looking for matches. If
-  // we add this route before any other routes, those routes will be unreachable
+  // Serve all static files that are contained in public
+  app.use(
+    express.static(path.join(__dirname, "../public"), {
+      extensions: ["html", "js", "css"]
+    })
+  );
+  // If no matching route is found default to home
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
