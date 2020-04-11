ce2d74b0335b0dd5cfffe557de06e5d28b2023e9
jkaplanis <johntkaplanis@gmail.com>
2020-04-10 19:13:13 -0700

    add additional comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,7 @@
 // Import the path package to get the correct file path for our html
 const path = require("path");
 const express = require("express");
-// Export a function that accepts an Express app object and sets up
-// html routes to server the html files.
+
 module.exports = app => {
   // Serve all static files that are contained in public
   app.use(
